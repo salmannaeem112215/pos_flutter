0a011aee8af9de6e5cf1227b9f2dcfd18b5002df
Salman <salman.naeem.112215@gmail.com>
2022-12-16 18:33:07 +0500

    Made a basic Responsive template

--- lib/bill/billScreen.dart
+++ lib/bill/billScreen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class BillScreen extends StatefulWidget {
   const BillScreen({Key? key}) : super(key: key);
@@ -13,10 +11,46 @@ class _BillScreenState extends State<BillScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).backgroundColor,
+      body: Container(
+        child: Column(children: [
+          Container(
+            height: 50,
+            color: Colors.amber,
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.black38,
+              child: Row(
+                children: [
+                  Flexible(
+                      flex: 3,
+                      child: Container(
+                        color: Colors.deepPurple,
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                color: Color.fromARGB(255, 53, 155, 67),
+                              ),
+                            ),
+                            Container(
+                              height: 150,
+                              color: Colors.deepOrangeAccent,
+                            ),
+                          ],
+                        ),
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        color: Color.fromARGB(255, 112, 99, 136),
+                      )),
+                ],
+              ),
+            ),
+          ),
+        ]),
       ),
-      body: Container(),
     );
   }
 }
